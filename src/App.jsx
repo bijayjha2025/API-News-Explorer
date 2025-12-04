884ad0afb8ae07acd793909fc1fc9ef6fb803451
Bijay Jha <bijayjha12345@gmail.com>
2025-12-04 19:45:41 +0545

    Functionality and API

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,59 @@
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import NavBar from './Components/NavBar.jsx';
 import NewsList from './Components/NewsList.jsx';
 import ErrorMessage from './Components/ErrorMessage.jsx';
 import LoadingMessage from './Components/LoadingMessage.jsx';
+import './App.css'
 
 
 function App() {
 
+  
   const [category, setCategory] = useState("general");
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        setLoading(true);
+        setError("");
+
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_NEWS_API}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.status !== "ok") {
+          throw new Error("Unable to fetch news");
+        }
+
+        setNews(data.articles);
+      }
+      catch (err) {
+        setError(err.message);
+        setNews([]);
+      }
+      finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNews();
+  }, [category]);
 
   return (
-    <>
-        
-    </>
+   <div>
+     <NavBar onCategoryChange = {setCategory}/>
+      <h2 className='categoryDisplay'>Category: {category} </h2>
+
+      {loading && <LoadingMessage />}
+      {error && <ErrorMessage message= {error}/>}
+
+      {!loading && !error && <NewsList articles={news} />}
+
+    </div>
   );
 }
 
