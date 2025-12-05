cfea16813a9327d9855b7637b5d916874e4cecb3
Bijay Jha <bijayjha12345@gmail.com>
2025-12-05 22:06:13 +0545

    Footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+    return(
+      <footer>
+       <p>© {new Date().getFullYear()} News Explorer_NewsAsTreasure. All rights reserved.</p>
+       <p>Bijay</p>
+       <p>News powered by <a href="https://newsapi.org/" target="_blank" rel="....">NewsAPI.org</a></p>
+    </footer>
+    );
+}
+
+
+export default Footer
