4f42907eff2b35c06d77ca2a8df41c3dc678f9f7
Bijay Jha <bijayjha12345@gmail.com>
2025-12-05 20:54:48 +0545

    Adding Logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,18 +2,22 @@
 
 function NavBar({onCategoryChange}){
 
-    const categories = ["general", "technology", "health", "business", "sports"];
+    const categories = ["general", "technology", "health", "business", "sports", "science", "entertainment"];
 
     return(
         <nav className="navbar">
-        {categories.map((categ) => (
-            <button className="buttons"
+            <div><img src="/Logo.png" alt="Logo" className="mainLogo" onClick={() => onCategoryChange("general")}/></div>
+
+            <div className="navbarButtons">
+            {categories.map((categ) => (
+            <button
             key={categ}
             onClick={() => onCategoryChange(categ)}>
 
             {categ.charAt(0).toUpperCase() + categ.slice(1)}</button>
            )
            )}
+           </div>
         </nav>
     );
 }
